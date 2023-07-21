7d9c1443b68d95768293174e9f49d690f519bcb1
Mack Talcott <mack@tal.co.tt>
2023-07-21 09:45:48 -0700

    Extract LoadingSpinner component

---
+++ client/src/components/LoadingSpinner.js
@@ -0,0 +1,5 @@
+import "./LoadingSpinner.css";
+
+export default function LoadingSpinner() {
+    return <span className="loader">Loading...</span>;
+}
--- client/src/components/pages/ActiveTaskPage.js
+++ client/src/components/pages/ActiveTaskPage.js
@@ -1,13 +1,12 @@
 import "./ActiveTaskPage.css";
 
-import { useEffect, useState, useRef } from "react";
+import { useState } from "react";
 import { useInterval } from "utils/useInterval";
 import { fetchAppJson } from "utils";
 import { Link } from "react-router-dom";
+import LoadingSpinner from "../LoadingSpinner";
 
 export default function ActiveTaskPage() {
-    // const { data, isLoading } = useFetch(appApiUrl("/api/active_task"));
-
     const [task, setTask] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
 
@@ -25,7 +24,7 @@ export default function ActiveTaskPage() {
         <>
             <p>
                 <span>Status: {task?.status || ""}</span>{" "}
-                {isLoading && <span className="loader">Loading...</span>}
+                {isLoading && <LoadingSpinner />}
             </p>
             <Link to="/task_options">Start over</Link>
             {task?.info && (
