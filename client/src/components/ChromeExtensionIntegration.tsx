db2de52fcc9fa9c689e46ea9f03f6734ab74ad34
Mack Talcott <mack@tal.co.tt>
2023-08-04 16:05:53 -0700

    Floating action button and info icon

--- client/src/components/ChromeExtensionIntegration.tsx
+++ client/src/components/ChromeExtensionIntegration.tsx
@@ -1,5 +1,31 @@
+import { css } from "@emotion/react";
+import Box from "@mui/material/Box";
+import Fab from "@mui/material/Fab";
+import Grow from "@mui/material/Grow";
+import InfoIcon from "@mui/icons-material/Info";
+import DeleteIcon from "@mui/icons-material/Delete";
 import { useContext, useEffect, useState } from "react";
 import { TaskResultsContext } from "utils/TaskResultsContext";
+import IconButton from "@mui/material/IconButton";
+import Slide from "@mui/material/Slide";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import Button from "@mui/material/Button";
+import Link from "@mui/material/Link";
+
+const styles = {
+  fabContainer: css({
+    position: "absolute",
+    bottom: 0,
+    right: 0,
+    padding: "16px",
+    display: "flex",
+    alignItems: "center",
+  }),
+};
 
 export default function ChromeExtensionIntegration() {
   const [isChromeExtensionFound, setIsChromeExtensionFound] = useState(false);
@@ -30,61 +56,94 @@ export default function ChromeExtensionIntegration() {
 
   const [isProcessing, setIsProcessing] = useState(false);
 
-  if (isChromeExtensionFound) {
-    return (
-      <>
-        <p>
-          <i>
-            Click "Delete duplicates" to delete the selected duplicates. This
-            will open up a new tab and delete the selected duplicate photos
-            through the Google Photos web app. Deleted photos can be{" "}
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://support.google.com/photos/answer/6128858#restore-items"
-            >
-              restored
-            </a>{" "}
-            for up to 60 days after they are deleted.
-          </i>
-        </p>
-        <p>
-          <button
-            className="processDuplicates"
-            onClick={() =>
-              processDuplicates({
-                results,
-                selectedGroups,
-                selectedOriginals,
-                setIsProcessing,
-              })
-            }
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const handleDialogOpen = () => {
+    setIsDialogOpen(true);
+  };
+
+  const handleDialogClose = () => {
+    setIsDialogOpen(false);
+  };
+
+  const handleProcessDuplicates = () => {
+    processDuplicates({
+      results,
+      selectedGroups,
+      selectedOriginals,
+      setIsProcessing,
+    });
+  };
+
+  return (
+    <>
+      <Box css={styles.fabContainer}>
+        <Grow in>
+          <IconButton
+            aria-label="info"
+            sx={{ mr: 1 }}
+            onClick={handleDialogOpen}
           >
+            <InfoIcon />
+          </IconButton>
+        </Grow>
+        <Grow in>
+          <Fab
+            variant="extended"
+            color="primary"
+            disabled={!isChromeExtensionFound || selectedCount <= 0}
+            onClick={handleProcessDuplicates}
+          >
+            <DeleteIcon sx={{ mr: 1 }} />
             Delete {selectedCount} duplicate
             {selectedCount !== 1 && "s"}
-          </button>
-        </p>
-      </>
-    );
-  } else {
-    return (
-      <>
-        <p>
-          <i>
-            Install the{" "}
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://github.com/mtalcott/google-photos-deduper/tree/main/chrome_extension/README.md"
-            >
-              Chrome Extension
-            </a>{" "}
-            to delete duplicates.
-          </i>
-        </p>
-      </>
-    );
-  }
+          </Fab>
+        </Grow>
+      </Box>
+      {isChromeExtensionFound ? (
+        <Dialog open={isDialogOpen} onClose={handleDialogClose}>
+          <DialogTitle>Deleting duplicates</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              Clicking the button will open up a new tab and delete the selected
+              duplicate photos through the Google Photos web app. Deleted photos
+              can be{" "}
+              <Link
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://support.google.com/photos/answer/6128858#restore-items"
+              >
+                restored
+              </Link>{" "}
+              for up to 60 days after they are deleted.
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleDialogClose}>Okay</Button>
+          </DialogActions>
+        </Dialog>
+      ) : (
+        <Dialog open={isDialogOpen} onClose={handleDialogClose}>
+          <DialogTitle>Install Chrome Extension</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              Install the{" "}
+              <Link
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://github.com/mtalcott/google-photos-deduper/tree/main/chrome_extension/README.md"
+              >
+                Chrome Extension
+              </Link>{" "}
+              to delete duplicates.
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleDialogClose}>Okay</Button>
+          </DialogActions>
+        </Dialog>
+      )}
+    </>
+  );
 }
 
 async function pingCheckChromeExtension() {
