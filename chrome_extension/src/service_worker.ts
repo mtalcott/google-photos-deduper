2ae1caa18b4c78b43e61120eae5fd821960e1d20
Mack Talcott <mack@tal.co.tt>
2023-08-25 07:02:47 -0700

    Smallest photos window possible

--- chrome_extension/src/service_worker.ts
+++ chrome_extension/src/service_worker.ts
@@ -54,6 +54,8 @@ function handleStartDeletionTask(
       const window = await chrome.windows.create({
         focused: true,
         incognito: sender.tab!.incognito,
+        height: 1,
+        width: 1,
       });
       const tab = window.tabs![0];
 
