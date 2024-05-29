a0f22951397d2b4f44f86c3f6e159c748cddf981
Mack Talcott <mack@tal.co.tt>
2024-05-28 22:51:39 -0700

    Specify host port for chrome extension vite server

--- chrome_extension/vite.config.ts
+++ chrome_extension/vite.config.ts
@@ -3,5 +3,9 @@ import { crx } from "@crxjs/vite-plugin";
 import manifest from "./manifest.json";
 
 export default defineConfig({
+  server: {
+    host: true,
+    port: 3001,
+  },
   plugins: [crx({ manifest })],
 });
