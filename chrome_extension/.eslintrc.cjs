54c40693766daed111d5d7b1a423123562a1eafb
Mack Talcott <mack@tal.co.tt>
2024-11-01 21:58:03 -0700

    Hide react/no-unknown-property lint errors

--- chrome_extension/.eslintrc.cjs
+++ chrome_extension/.eslintrc.cjs
@@ -14,6 +14,6 @@ module.exports = {
       "warn",
       { allowConstantExport: true },
     ],
-    "react/no-unknown-property": ["error", { ignore: ["css"] }],
+    "react/no-unknown-property": "off",
   },
 };
