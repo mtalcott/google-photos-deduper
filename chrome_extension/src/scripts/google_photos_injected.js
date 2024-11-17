0108cc65cbb08521636cfad69dd1674ae6f503a7
Mack Talcott <mack@tal.co.tt>
2024-11-17 14:57:03 -0800

    Comment explaining google_photos_injected

--- chrome_extension/src/scripts/google_photos_injected.js
+++ chrome_extension/src/scripts/google_photos_injected.js
@@ -1,3 +1,13 @@
+// Runs on Google Photos web app pages.
+// Because this script is added directly
+//   to the page, it has access to window variables that a Chrome extension
+//   content script does not (see https://developer.chrome.com/docs/extensions/reference/manifest/content-scripts#world-timings).
+//   This is necessary to be able to access GPTK's `window.gptkApi`.
+// Unfortunately, CRXJS doesn't support compilation of web_accessible_resources
+//   (see https://github.com/crxjs/chrome-extension-tools/discussions/616), and
+//   also doesn't support `world: "MAIN"` (see https://github.com/crxjs/chrome-extension-tools/issues/695)
+//   so JS and not TS it is.
+
 // Listen for incoming messages on window from servaice worker executeScript calls
 window.addEventListener("message", async (message) => {
   if (message.data?.app !== "GooglePhotosDeduper") {
